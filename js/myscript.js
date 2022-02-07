57b0dedfb15d8a2e17a56786c971a06b004e68a8
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-07 17:29:37 +0100

    .

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,16 @@
-for (let i = 1; i < 101; i++) {
+for (let i = 1; i <= 100; i++) {
   console.log(i)
 
-  const li = document.createElement('li')
-  li.append(i)
+  let ul = document.getElementById('list');
+  let li = document.createElement('li');
+  li.innerHTML = `${i}`;
+  ul.append(li);
+
+  if (i % 3 === 0 ) {
+    li.innerHTML = `Frizz`;
+  } else if (i % 5 === 0 ){
+    li.innerHTML = `Buzz`;
+  } else if (i % (5 + 3) === 0 ){
+    li.innerHTML = `FrizzBuzz`;
+  }
 }
